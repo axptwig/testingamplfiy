214d2d46f16ce6d0733310158c83d8ec244e934e
axptwig <raghuraman@outlook.com>
2019-09-27 12:43:56 -0700

    adding themeing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GridView from './../GridView';
+import Navbar from './../Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <GridView></GridView>
       </header>
